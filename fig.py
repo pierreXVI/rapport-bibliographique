0c0c28522ab635a7a26d5e3997f1087fb9e22df2
Pierre Seize <pierreseize@gmail.com>
2020-06-24 13:10:43 +0200

    Work

--- fig.py
+++ fig.py
@@ -38,5 +38,51 @@ def fig_rk():
     plt.show()
 
 
+def fig_bdf():
+    def r(k):
+        def sub_r(z):
+            coeff = np.zeros(k + 1, dtype=complex)
+            for i in range(1, k + 1):
+                coeff[0] += 1 / i
+                coeff[i] = np.power(-1, i) * scipy.special.binom(k, i) / i
+            coeff[0] -= z
+            return (np.abs(np.roots(coeff)) < 1).all()
+
+        return np.vectorize(sub_r)
+
+    fig = plt.figure()
+    c = [.8, .0, .0, 1]
+    for (i, x_min, x_max, y_min, y_max) in zip((1, 2, 3, 4, 5, 6),
+                                               (-2, -2, -4, -4, -10, -20),
+                                               (3, 5, 8, 14, 25, 40),
+                                               (-2, -3, -5, -8, -15, -30),
+                                               (2, 3, 5, 8, 15, 30),
+                                               ):
+        res_x = (x_max - x_min) / 500
+        res_y = (y_max - y_min) / 500
+
+        x = np.arange(x_min, x_max, res_x)
+        y = np.arange(y_min, y_max, res_y)
+        x, y = np.meshgrid(x, y)
+
+        ax = fig.add_subplot(2, 3, i)
+        ax.set_aspect('equal')
+        for axis in ('left', 'bottom'):
+            ax.spines[axis].set_position('zero')
+            ax.spines[axis].set_linewidth(2)
+        for axis in ('right', 'top'):
+            ax.spines[axis].set_color('none')
+        ax.tick_params(width=2, labelsize=12)
+        ax.yaxis.tick_left()
+        ax.xaxis.tick_bottom()
+        rk_i = r(i)(x + 1j * y)
+        ax.pcolormesh(x, y, np.ma.masked_array(rk_i, mask=1 - rk_i), cmap=clr.ListedColormap([c]))
+        ax.plot([None], [None], '--', c=c, lw=20)
+        ax.grid(True, lw=2)
+        ax.set_title('BDF{0}'.format(i), fontsize=16, y=-.1)
+
+    plt.show()
+
+
 if __name__ == '__main__':
     fig_rk()
