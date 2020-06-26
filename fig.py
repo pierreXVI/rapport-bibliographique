b6d49060b087c582a08873db89d338761a67abf1
Pierre Seize <pierre.seize@onera.fr>
2020-06-26 13:56:36 +0200

    Changing pics

--- fig.py
+++ fig.py
@@ -1,18 +1,38 @@
+import matplotlib
 import matplotlib.colors as clr
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.linalg
 import scipy.special
+
+matplotlib.rcParams.update({
+    'xtick.labelsize': 15,
+    'ytick.labelsize': 15,
+    'xtick.major.width': 1.5,
+    'xtick.minor.width': 1,
+    'ytick.major.width': 1.5,
+    'ytick.minor.width': 1,
+    'axes.grid': True,
+    'axes.labelsize': 20,
+    'axes.titlesize': 20,
+    'grid.linewidth': 2,
+    'legend.fontsize': 20,
+    'legend.labelspacing': 1,
+    'lines.markeredgewidth': 15,
+    'lines.markersize': 3,
+    'lines.linewidth': 3,
+    'text.latex.preamble': [r"\usepackage{amsmath}"],
+    'text.usetex': True
+})
 
 
 def fig_rk():
     x_min, x_max = -4, 2
     y_min, y_max = -4, 4
-    res_x = 0.01
-    res_y = 0.01
+    res_x = 700
+    res_y = 700
 
-    x = np.arange(x_min, x_max, res_x)
-    y = np.arange(y_min, y_max, res_y)
-    x, y = np.meshgrid(x, y)
+    x, y = np.meshgrid(np.linspace(x_min, x_max, res_x), np.linspace(y_min, y_max, res_y))
 
     def r(k):
         return np.vectorize(lambda z: sum(np.power(z, range(k + 1)) / scipy.special.factorial(range(k + 1))))
@@ -25,16 +45,16 @@ def fig_rk():
         ax.spines[axis].set_linewidth(2)
     for axis in ('right', 'top'):
         ax.spines[axis].set_color('none')
-    ax.tick_params(width=2, labelsize=12)
     ax.yaxis.tick_left()
     ax.xaxis.tick_bottom()
     for (i, c) in zip((4, 3, 2, 1), ([.8, .0, .0, 1], [.0, .8, .0, 1], [.0, .0, .8, 1], [.5, .5, .5, 1])):
         rk_i = abs(r(i)(x + 1j * y)) < 1
         ax.pcolormesh(x, y, np.ma.masked_array(rk_i, mask=1 - rk_i), cmap=clr.ListedColormap([c]))
-        ax.plot([None], [None], '--', c=c, lw=20, label='RK{0}'.format(i))
-    ax.grid(True, lw=2)
-    ax.legend(fontsize=16, loc='upper left', handletextpad=1, labelspacing=1)
+        ax.plot([None], '--', c=c, lw=20, label='RK{0}'.format(i))
 
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles[::-1], labels[::-1], loc='upper left')
+    ax.grid()
     plt.show()
 
 
@@ -58,12 +78,10 @@ def fig_bdf():
                                                (-2, -3, -5, -8, -15, -30),
                                                (2, 3, 5, 8, 15, 30),
                                                ):
-        res_x = (x_max - x_min) / 500
-        res_y = (y_max - y_min) / 500
+        res_x = 700
+        res_y = 700
 
-        x = np.arange(x_min, x_max, res_x)
-        y = np.arange(y_min, y_max, res_y)
-        x, y = np.meshgrid(x, y)
+        x, y = np.meshgrid(np.linspace(x_min, x_max, res_x), np.linspace(y_min, y_max, res_y))
 
         ax = fig.add_subplot(2, 3, i)
         ax.set_aspect('equal')
@@ -72,14 +90,13 @@ def fig_bdf():
             ax.spines[axis].set_linewidth(2)
         for axis in ('right', 'top'):
             ax.spines[axis].set_color('none')
-        ax.tick_params(width=2, labelsize=12)
         ax.yaxis.tick_left()
         ax.xaxis.tick_bottom()
         rk_i = r(i)(x + 1j * y)
         ax.pcolormesh(x, y, np.ma.masked_array(rk_i, mask=1 - rk_i), cmap=clr.ListedColormap([c]))
-        ax.plot([None], [None], '--', c=c, lw=20)
-        ax.grid(True, lw=2)
-        ax.set_title('BDF{0}'.format(i), fontsize=16, y=-.1)
+        ax.plot([None], '--', c=c, lw=20)
+        ax.set_title('BDF{0}'.format(i), y=-.1)
+        ax.grid()
 
     plt.show()
 
@@ -87,12 +104,10 @@ def fig_bdf():
 def fig_ab():
     x_min, x_max = -2.5, .5
     y_min, y_max = -1.5, 1.5
-    res_x = 500
-    res_y = 500
+    res_x = 700
+    res_y = 700
 
-    x = np.linspace(x_min, x_max, res_x)
-    y = np.linspace(y_min, y_max, res_y)
-    x, y = np.meshgrid(x, y)
+    x, y = np.meshgrid(np.linspace(x_min, x_max, res_x), np.linspace(y_min, y_max, res_y))
 
     def lbd(i, n):
         coeff = np.ones(1)
@@ -130,15 +145,73 @@ def fig_ab():
         ax.spines[axis].set_linewidth(2)
     for axis in ('right', 'top'):
         ax.spines[axis].set_color('none')
-    ax.tick_params(width=2, labelsize=12)
     ax.yaxis.tick_left()
     ax.xaxis.tick_bottom()
     for (i, c) in zip((1, 2, 3, 4), ([.5, .5, .5, 1], [.0, .0, .8, 1], [.0, .8, .0, 1], [.8, .0, .0, 1],)):
         rk_i = r(i)(x + 1j * y)
         ax.pcolormesh(x, y, np.ma.masked_array(rk_i, mask=1 - rk_i), cmap=clr.ListedColormap([c]))
-        ax.plot([None], [None], '--', c=c, lw=20, label='RK{0}'.format(i))
-    ax.grid(True, lw=2)
-    ax.legend(fontsize=16, loc='upper left', handletextpad=1, labelspacing=1)
+        ax.plot([None], '--', c=c, lw=20, label='AB{0}'.format(i))
+    ax.legend(loc='upper left')
+    ax.grid()
+
+    plt.show()
+
+
+def fig_precond():
+    from scipy.sparse.linalg import gmres as gmres_scipy
+
+    class GMRESCounter:
+        def __init__(self, res0=1):
+            self.niter = 0
+            self.res = [res0]
+
+        def __call__(self, rk):
+            self.res.append(rk)
+            self.niter += 1
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(121)
+    ax2 = fig.add_subplot(122)
+    ax1.set_aspect('equal')
+    ax2.set_xlabel(r'$n_{iter}$')
+    ax2.set_ylabel(r'$\frac{||r_n||}{||r_0||}$', rotation=0)
+
+    n = 200
+    angle = 2 * np.arange(n) * np.pi / (n - 1)
+    a = np.diagflat(2 * np.sin(angle) - 1 + 1j * np.cos(angle)) + np.random.normal(size=(n, n)) / (2 * np.sqrt(n))
+
+    val, vec = scipy.linalg.eig(a)
+    cond_a = np.linalg.cond(a)
+    cond_v = np.linalg.cond(vec)
+    ax1.plot(np.real(val), np.imag(val), 'x', mew=1, ms=6)
+
+    counter = GMRESCounter()
+    gmres_scipy(a, np.ones(n), callback=counter, maxiter=12)
+    l1, = ax2.semilogy(counter.res, '+')
+
+    jacobi = scipy.sparse.diags(1 / a.diagonal())
+
+    a_prec = a @ jacobi
+    val, vec = scipy.linalg.eig(a_prec)
+    cond_a_pre = np.linalg.cond(a_prec)
+    cond_v_pre = np.linalg.cond(vec)
+    ax1.plot(np.real(val), np.imag(val), '+', mew=1, ms=6)
+
+    counter = GMRESCounter()
+    gmres_scipy(a_prec, np.ones(n), callback=counter, maxiter=12)
+    l2, = ax2.semilogy(counter.res, 'x')
+
+    leg = ax2.legend([l1, l2], ['', ''], handletextpad=16, loc='lower center', bbox_to_anchor=(-0.75, 0.0))
+
+    ax2.text(-1, 0.035,
+             r'$\begin{{aligned}}'
+             r'\kappa\left(A\right) &= {0:0.2f} & \kappa\left(V\right) &= {1:0.2f} \\[10pt]'
+             r'\kappa\left(A_{{pre}}\right) &= {2:0.2f} \quad& \kappa\left(V_{{pre}}\right) &= {3:0.2f} \\'
+             r'\end{{aligned}}$'.format(cond_a, cond_v, cond_a_pre, cond_v_pre),
+             transform=ax2.transAxes,
+             zorder=leg.zorder + 1,
+             fontsize=matplotlib.rcParams['legend.fontsize']
+             )
 
     plt.show()
 
